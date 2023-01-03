4d3b12cf29c1a0c76a53173ed90a4ef381869557
phongsathorn <phongsathorn_nil@cmu.ac.th>
2023-01-03 16:10:36 +0700

    lab11_commit

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,25 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    int N;
+    double sum, SS;
+    string textline;
+    ifstream source("score.txt");
+    while(getline(source,textline))
+    {
+        N++;
+        sum += atof(textline.c_str());
+        SS += atof(textline.c_str())*atof(textline.c_str());
+    }
+    cout << "Number of data = " << N << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << sum/N << "\n";
+    cout << "Standard deviation = " << sqrt(1.0/N*SS-sum*sum/N/N) << "\n";
+}
